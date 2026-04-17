50b9038fc1825975b63abe38e4b21ffc84066ba8
softgenai <admin@softgen.ai>
2026-04-17 17:43:03 +0000

    fix(app): wrap application in upload provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
+import { UploadProvider } from "@/contexts/UploadContext";
 import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="dribblestats-theme">
-      <Component {...pageProps} />
-      <Toaster />
+      <UploadProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </UploadProvider>
     </ThemeProvider>
   );
 }
